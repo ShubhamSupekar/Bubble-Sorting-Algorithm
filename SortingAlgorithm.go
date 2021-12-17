60d92120f5f50b9f8418cd04a590e0580c3d1637
Shubham Supekar <89061941+ShubhamSupekar@users.noreply.github.com>
2021-12-17 15:57:13 +0530

    Add files via upload
    1st file is using func main and 2nd file is using func sort

---
+++ SortingAlgorithm.go
@@ -0,0 +1,19 @@
+package main
+import(
+	"fmt"
+)
+func main(){
+	a := []int{10,40,100,20,60}
+	for n:=1;n<=3;n++{   //for multiple sorting to get precision result
+		for i:=0;i<len(a)-n;i++{   //for finding max number only
+			if a[i]>a[i+1]{
+				temp := a[i]
+				a[i] = a[i+1]
+				a[i+1] = temp
+			}
+		}  
+	fmt.Println("max number is:",a[len(a)-1])
+	fmt.Println(a)
+	}
+	fmt.Println("final value is:",a)
+}
